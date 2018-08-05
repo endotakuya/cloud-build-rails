84c62bbcf454c6d29204b485ae4ff20d5efdb430
enta0701 <endo.takuya.0701@gmail.com>
2018-08-06 02:48:14 +0900

    age: テストの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   it "成人かどうかが分かること" do
-    user = User.new(name: "enta", age: 18)
+    user = User.new(name: "enta", age: 25)
     expect(user.adult?).to eq true
   end
 end
